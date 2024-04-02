605ecd34350049287c4918ad817c988d85069cb3
BohdanLysniak <marttelo@ukr.net>
2024-04-02 18:42:47 +0300

    task-done

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,14 +3,13 @@ import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const startButton = document.querySelector([data - start]);
+const startButton = document.querySelector("[data-start]");
 startButton.disabled = true;
+const daysTimer = document.querySelector("[data-days]");
+const hoursTimer = document.querySelector("[data-hours]");
+const minutesTimer = document.querySelector("[data-minutes]");
+const secondsTimer = document.querySelector("[data-seconds]");
 
-startButton.addEventListener("click", startTimmer);
-
-function startButton() {
-  
-}
 
 let userSelectedDate;
 
@@ -34,4 +33,41 @@ flatpickr("#datetime-picker", {
     }
   },
 });
+
+
+startButton.addEventListener("click", startTimer);
+
+function startTimer() {
+  setInterval(() => {
+    let remainerOfTime = userSelectedDate.getTime() - Date.now();
+    daysTimer.innerHTML = convertMs(remainerOfTime).days;
+    hoursTimer.innerHTML = convertMs(remainerOfTime).hours;
+    minutesTimer.innerHTML = convertMs(remainerOfTime).minutes;
+    secondsTimer.innerHTML = convertMs(remainerOfTime).seconds;
+  }, 1000)
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
 
